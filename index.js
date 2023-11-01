78d58f4bcff4caaf0746bb7b7e69a6ba03f58e74
Max Ganiyev <maxganiyev@gmail.com>
2023-11-01 23:20:03 +0100

    exec

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const { exec } = require("child_process");
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node new!\n'
-  res.end(msg);
+    exec("ls -la", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`stdout: ${stdout}`);
+    });
 });
 
 server.listen(port, () => {
