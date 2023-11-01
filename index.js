40ac40e2de16868c859dae2887f6ac48169aa4fa
Max Ganiyev <maxganiyev@gmail.com>
2023-11-02 00:12:34 +0100

    sd

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
     if (req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
-          res.write(files.filetoupload.filepath);
+          res.write(JSON.stringify(files));
 
         //   exec("./oidn/bin/oidnDenoise --hdr ./oidn/img/noise.pfm -o ./oidn/img/denoise.pfm", (error, stdout, stderr) => {
         //     if (error) {
