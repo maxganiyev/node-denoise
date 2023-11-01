40e1793e820c5a5f34ff23e767cc108057b83c47
Max Ganiyev <maxganiyev@gmail.com>
2023-11-01 23:50:53 +0100

    file upload

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 const { exec } = require("child_process");
+var formidable = require('formidable');
 
 const server = http.createServer((req, res) => {
-    exec("./oidn/bin/oidnDenoise --hdr ./oidn/img/noise.pfm -o ./oidn/img/denoise.pfm", (error, stdout, stderr) => {
-        if (error) {
-            res.statusCode = 500;
-            res.end(`error: ${error.message}` + `stdout: ${stdout}` + `stderr: ${stderr}`);
-            return;
-        }
+    if (req.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+          res.write(files + '\n');
 
-        if (stderr) {
-            res.statusCode = 500;
-            res.end(`stderr: ${stderr}` + `stdout: ${stdout}` + `error: ${error.message}`);
-            return;
-        }
+        //   exec("./oidn/bin/oidnDenoise --hdr ./oidn/img/noise.pfm -o ./oidn/img/denoise.pfm", (error, stdout, stderr) => {
+        //     if (error) {
+        //         res.statusCode = 500;
+        //         res.end(`error: ${error.message}` + `stdout: ${stdout}` + `stderr: ${stderr}`);
+        //         return;
+        //     }
+    
+        //     if (stderr) {
+        //         res.statusCode = 500;
+        //         res.end(`stderr: ${stderr}` + `stdout: ${stdout}` + `error: ${error.message}`);
+        //         return;
+        //     }
+    
+        //     res.statusCode = 200;
+        //     //res.setHeader('Content-Type', 'text/plain');
+        //     res.end(`stdout: ${stdout}`);
+        // });
 
-        res.statusCode = 200;
-        //res.setHeader('Content-Type', 'text/plain');
-        res.end(`stdout: ${stdout}`);
-    });
+          res.end();
+        });
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="filetoupload"><br>');
+        res.write('<input type="submit">');
+        res.write('</form>');
+        return res.end();
+    }
+    
 });
 
 server.listen(port, () => {
